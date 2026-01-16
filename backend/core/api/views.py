b2bd385ad3536fb43628e13dcd6ddba74995c00c
BLACHERE Nicolas <nicolas.blachere@gmail.com>
2026-01-16 09:03:50 +0100

    fixing the error

--- backend/core/api/views.py
+++ backend/core/api/views.py
@@ -4,7 +4,6 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
-from datetime import datetime  # Unused import to test CI
 
 from .models import Order, Product
 from .serializers import AdminOrderSerializer, OrderSerializer, ProductSerializer
