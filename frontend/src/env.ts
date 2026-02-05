178bf2bba831752c658276b3638e8fd5dbb4626f
BLACHERE Nicolas <nicolas.blachere@gmail.com>
2026-02-05 16:33:09 +0100

    plop

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,7 +1,5 @@
 const DEFAULT_HOST = "35.180.57.114";
 
-export const API_BASE =
-	import.meta.env.VITE_API_BASE ?? `http://${DEFAULT_HOST}:5000/api`;
+export const API_BASE = `http://${DEFAULT_HOST}:5000/api`;
 
-export const ADMIN_BASE_URL =
-	import.meta.env.VITE_ADMIN_BASE ?? `http://${DEFAULT_HOST}:5000/admin/`;
+export const ADMIN_BASE_URL = `http://${DEFAULT_HOST}:5000/admin/`;
