9a4224b322b614d7b58f93326e34235dba8d9fd4
BLACHERE Nicolas <nicolas.blachere@gmail.com>
2026-02-05 16:32:52 +0100

    on met l'ip en dur parce qu'on est des fous

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,4 +1,4 @@
-const DEFAULT_HOST = typeof window !== "undefined" ? window.location.hostname : "localhost";
+const DEFAULT_HOST = "35.180.57.114";
 
 export const API_BASE =
 	import.meta.env.VITE_API_BASE ?? `http://${DEFAULT_HOST}:5000/api`;
